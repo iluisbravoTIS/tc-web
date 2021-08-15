472556fac1deb03fd2a63ab8f3b1f11e89cf9476
carlosmiker <carlosmiker73@gmail.com>
2021-08-15 11:59:37 -0700

    Cambios al deal/service

--- src/providers/deals.js
+++ src/providers/deals.js
@@ -1,35 +1,38 @@
 const Deal = (() => {
 
-    const apiURL = 'http://7053b52ae2d6.ngrok.io/contact'
+    const apiURL = 'https://tc-webapi.herokuapp.com'
     return {
-        New: async (contact) => {
-            const request = 
-            {
-                dealstage,
-                pipeline,
-                dealname,
-                parte_del_cuerpo_a_tatuar,
-                alto_del_tatuaje,
-                ancho_del_tatuaje,
-                color_de_tatuaje,
-                comentarios_adicionales,
-                foto_del_tatuaje,
-                color_de_piel
-            };
-            const httpMethod = 
-            {
-                method: 'POST',
-                body: JSON.stringify(request),
-                headers:{
-                    'content-type': 'application/json',
-                    'accept': 'application/json'
-                }
-            };
-            const url = {apiURL}
-            const result = await fetch();
-        },
-        UploadImage: async () => {
-            
+        New: async (contactid, {dealname, parte_del_cuerpo_a_tatuar, alto_del_tatuaje, ancho_del_tatuaje, color_de_tatuaje, comentarios_adicionales, color_de_piel, foto_del_tatuaje}) => {
+            try{
+                const request = 
+                {
+                    dealstage: '1272076',
+                    pipeline: 'default',
+                    dealname,
+                    parte_del_cuerpo_a_tatuar,
+                    alto_del_tatuaje,
+                    ancho_del_tatuaje,
+                    color_de_tatuaje,
+                    comentarios_adicionales,
+                    color_de_piel,
+                    foto_del_tatuaje
+                };
+                const httpMethod = 
+                {
+                    method: 'POST',
+                    body: request,
+                    headers:{
+                        'content-type': 'application/json',
+                        'accept': 'application/json'
+                    }
+                };
+                const url = `${apiURL}/deal/${contactid}`;
+                const result = await fetch(url, httpMethod);
+                return result;
+            }catch(e){
+                console.error(e);
+                return { isOk=false, message: e.message};
+            }
         }
     }
 

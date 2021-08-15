1d4d41152c93d601ee1c093ded43cbc02a36803a
iluisbravotis <ing.luisalbertobravo@gmail.com>
2021-08-15 15:10:32 -0700

    finalizando datos

--- src/providers/deals.js
+++ src/providers/deals.js
@@ -2,9 +2,9 @@ const Deal = (() => {
 
     const apiURL = 'https://tc-webapi.herokuapp.com'
     return {
-        New: async (contactid, {dealname, parte_del_cuerpo_a_tatuar, alto_del_tatuaje, ancho_del_tatuaje, color_de_tatuaje, comentarios_adicionales, color_de_piel, foto_del_tatuaje}) => {
-            try{
-                const request = 
+        New: async (contactid, { dealname, parte_del_cuerpo_a_tatuar, alto_del_tatuaje, ancho_del_tatuaje, color_de_tatuaje, comentarios_adicionales, color_de_piel, foto_del_tatuaje }) => {
+            try {
+                const request =
                 {
                     dealstage: '1272076',
                     pipeline: 'default',
@@ -17,26 +17,29 @@ const Deal = (() => {
                     color_de_piel,
                     foto_del_tatuaje
                 };
-                const httpMethod = 
+                const httpMethod =
                 {
                     method: 'POST',
-                    body: request,
-                    headers:{
+                    body: JSON.stringify(request),
+                    headers: {
                         'content-type': 'application/json',
                         'accept': 'application/json'
                     }
                 };
                 const url = `${apiURL}/deal/${contactid}`;
                 const result = await fetch(url, httpMethod);
-                return result;
-            }catch(e){
+                const resp = await result.json();
+                return resp;
+            } catch (e) {
                 console.error(e);
-                return { isOk=false, message: e.message};
+                return { isOk: false, message: e.message };
             }
         }
     }
 
 })();
+
+export default Deal;
 
 
 // funcion para buscar si se debe mostrar el boton cotizar

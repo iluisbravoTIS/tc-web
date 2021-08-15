5c8f9c2aaa6a1cf2d5b2feb0fde1a05ab7c4482d
Kevin Urias <kevin.urias@tisconsulting.org>
2021-08-15 12:05:14 -0700

    Cambios en StepReferencia

--- src/components/Cotizar/StepReferencia.js
+++ src/components/Cotizar/StepReferencia.js
@@ -1,16 +1,44 @@
+import { Button } from '@material-ui/core';
 import React from 'react';
+import styled from 'styled-components';
+
+const Styles = styled.div`
+    .hide{         
+        display:none;       
+    }
+`;
 
 const StepReferencia = () => {
 
-    const [img, setImg] = React.useState();
+    const refButton = React.useRef();
+    const [file, setFile] = React.useState();
+    const [fileName, setNameFile] = React.useState("Selecciona imagen");
+
+    const onclickFile = () => {
+        refButton.current.click()
+    }
+
+    const handleFileUpload = event => {
+        console.log(event.target.files[0].name);
+        let nameFile = event.target.files[0].name;
+        setNameFile(event.target.files[0].name)
+    };
 
 
     return (
         <>
-            <div >
-                <p>Ingrese una imagén de referencia</p>
-                <input type="file" src={img}></input>
-            </div>
+            <Styles>
+                <div >
+                    <p>Ingrese una imagén de referencia</p>
+                    <input type="file" className="hide" ref={refButton} onChange={handleFileUpload}></input>
+                    { fileName }
+                    <br />
+                    <Button variant="contained" onClick={
+                        onclickFile
+                    }>Subir Imagen</Button>
+                </div>
+            </Styles>
+
         </>
     )
 }

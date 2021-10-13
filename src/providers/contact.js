3b41c71dc4f2f749ae612705bbdd7bba5e1fe793
iluisbravotis <ing.luisalbertobravo@gmail.com>
2021-10-13 12:13:55 -0700

    arreglando path de la api

--- src/providers/contact.js
+++ src/providers/contact.js
@@ -2,23 +2,22 @@ import validator from 'validator'
 
 
 
-const Contacts = (() =>{
-    
+const Contacts = (() => {
     const apiURL = 'https://tc-webapi.herokuapp.com/contact'
     return {
-        New: async ({firstname, lastname, phone, email, confirmacion_de_edad}) =>{
+        New: async ({ firstname, lastname, phone, email, confirmacion_de_edad }) => {
             //Validaciones
-            if(!firstname)
-                return { isOk: false, message: 'No se ha especificado el primer nombre'};
-            if(!lastname)
-                return { isOk: false, message: 'No se ha especificado el apellido'};
-            if(!validator.isMobilePhone(phone))
-                return { isOk: false, message: 'Ingrese un telefono valido'};
-            if(!validator.isEmail(email)) 
-                return { isOk: false, message: 'Ingrese un email valido'};
-            if(!confirmacion_de_edad)
-                return { isOk: false, message: 'No ha confirmado su edad'};
-    
+            if (!firstname)
+                return { isOk: false, message: 'No se ha especificado el primer nombre' };
+            if (!lastname)
+                return { isOk: false, message: 'No se ha especificado el apellido' };
+            if (!validator.isMobilePhone(phone))
+                return { isOk: false, message: 'Ingrese un telefono valido' };
+            if (!validator.isEmail(email))
+                return { isOk: false, message: 'Ingrese un email valido' };
+            if (!confirmacion_de_edad)
+                return { isOk: false, message: 'No ha confirmado su edad' };
+
             const data = {
                 firstname,
                 lastname,
@@ -29,45 +28,45 @@ const Contacts = (() =>{
 
             const url = `${apiURL}`;
             var params = {
-                 method: 'POST',
-                 body: JSON.stringify(data),
-                 headers: {
+                method: 'POST',
+                body: JSON.stringify(data),
+                headers: {
                     "content-type": 'application/json'
                 },
                 // mode: 'no-cors',
-                cache: 'default' 
+                cache: 'default'
             };
 
             const rawResponse = await fetch(url, params);
-            if(rawResponse.status == 200){
+            if (rawResponse.status == 200) {
                 // const {body} = rawRespose;
                 // console.log(body.json());
                 const responseData = await rawResponse.json();
                 const result = responseData;
                 return result;
             }
-            return { isOk: false};
+            return { isOk: false };
 
         },
-        GetByEmail: async (email) =>{
-            try{
+        GetByEmail: async (email) => {
+            try {
                 const url = `${apiURL}/ByEmail/${email}`;
                 const rawResponse = await fetch(url);
                 const responseData = await rawResponse.json();
                 const response = responseData;
                 return response;
-            }catch(e){
+            } catch (e) {
                 console.error(e);
             }
         },
-        GetByPhone: async (phone) =>{
-            try{
+        GetByPhone: async (phone) => {
+            try {
                 const url = `${apiURL}/ByPhone/${phone}`;
                 const rawResponse = await fetch(url);
                 const responseData = await rawResponse.json();
                 const response = responseData;
                 return response;
-            }catch(e){
+            } catch (e) {
                 console.error(e);
             }
         }
